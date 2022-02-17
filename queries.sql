ec46a484ae80a769b65835d9c024733800778612
LisaUofM <96268895+LisaUofM@users.noreply.github.com>
2022-02-16 20:33:43 -0600

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,1 +1,51 @@
----Queries
+--Queries
+
+
+select * FROM departments;
+select * FROM dept_emp;
+select * FROM dept_manager;
+select * FROM employees;
+select * FROM salaries; 
+select * FROM titles;
+
+alter table titles drop constraint pkey
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT emp_no, count(*)
+FROM employees;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31'
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31'
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31'
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31'
+
+-- Retirement eligibility
+SELECT COUNT(first_name)
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+Select * FROM retirement_info; 
+
+
