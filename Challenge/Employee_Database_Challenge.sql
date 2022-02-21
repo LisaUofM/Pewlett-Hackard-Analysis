a5b98acdf4dac084d415176814bb6b30a05a6392
LisaUofM <96268895+LisaUofM@users.noreply.github.com>
2022-02-20 20:42:42 -0600

    Challenge_2_mentorship_eligibility

--- Challenge/Employee_Database_Challenge.sql
+++ Challenge/Employee_Database_Challenge.sql
@@ -35,3 +35,21 @@ ORDER BY count DESC;
 
 --select * from retiring_titles
 
+--Deliverable 2: 
+SELECT DISTINCT on (emp_no) e.emp_no,
+     e.first_name,
+     e.last_name,
+	 e.birth_date,
+	 de.from_date,
+	 de.to_date,
+	 ti.title
+INTO MENT_EL
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as ti
+		ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01') AND 
+      (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no
+
